b1da3a74f0ba4bdd5f7b31f092d42d505221562e
hikaru zhen <jinghuizhen@outlook.jp>
2022-04-20 23:35:40 +0900

    [init]create first project outline with repository

---
+++ demo/src/test/java/com/hika/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hika.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
